2bceac9499aca2dc51e454579c5faf8d8c6ab3ed
Ale Villarroel <alejandrovillarroel394@gmail.com>
2024-06-03 19:29:57 -0300

    el trabajo casi terminado

---
+++ Clase javascrpti/main.js
@@ -0,0 +1,113 @@
+/* Clase 13: intro JS*/
+
+
+
+//Hola mundo desde un archivo externo
+console.log ("Hola Mundo");
+
+
+//variables
+
+// var nombre = "Harry"
+// console.log(nombre)
+
+// let nombreLe ="Hermione" //string
+// console.log(nombreLe)
+
+// let edad1 = 10 ; //numerico
+
+// const PI = 3.14;
+
+// let tenerMascotas1 = true; //
+
+// console.log(edad1,PI,tenerMascotas1)
+
+
+
+
+// let nombreLet = "Alejandro"
+// console.log(nombreLet)
+
+// let apellidoLet = "Villarroel"
+// console.log(apellidoLet)
+
+// let edad = 29;
+// console.log(edad)
+
+// let tenerMascotas = "True"
+// console.log(tenerMascotas)
+
+
+
+
+// //Operaciones basicas
+
+
+// let numeroA = 2
+// let numeroB = 3
+// let numeroC = 4
+
+// let resultadoSuma = numeroA + numeroB + numeroC
+
+// console.log(numeroA + numeroC)
+// console.log(resultadoSuma)
+
+
+
+// let nombre2 = "Cosme"
+// let apellido2 = "Fulanito"
+// const ESPACIO = " "
+
+// let nombreCompleto = nombre2 + ESPACIO + apellido2
+
+// console.log(nombreCompleto)
+
+
+
+
+//Prompt
+
+
+let entrada = prompt ("Ingrese su nombre")
+let salida = "Hola " + entrada
+alert (salida)
+
+
+
+let numero = parseInt(prompt ("Ingrese su numero"))
+const NUMEROB = 30;
+
+let salidaNumero = numero + NUMEROB
+alert (salidaNumero)
+
+
+
+
+//arrays
+
+
+let numeros = [1,2,4,5,8];
+let colores = ["Rojo", "verde", "Azul"];
+
+console.log(numero[1]);
+console.log(colores[0])
+
+
+
+//multidimencionales
+
+let colorMascotas = [
+    ["Perro", "Marron", " Gris" ],
+    ["Jirafa", "Blanco", " Negro" ],
+    ["gato" , "amarillo"]
+]
+
+console.log(colorMascotas [0] [2]);
+console.log(colorMascotas[1][1].length)
+
+
+
+//Tarea
+
+
+let nombres
